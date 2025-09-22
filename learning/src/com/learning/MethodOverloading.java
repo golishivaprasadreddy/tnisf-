0223e8b046b11e0dd8a62cf05079cf498e5d14ec
golishivaprasadreddy <gspreddy6869@gmail.com>
2025-09-22 21:35:31 +0530

    oops concepts

---
+++ learning/src/com/learning/MethodOverloading.java
@@ -0,0 +1,25 @@
+package com.learning;
+
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(5, 10));      
+        System.out.println(calc.add(3.5, 2.5));  
+    }
+}
+
+
+
+//output 
+//
+//15
+//6.0
